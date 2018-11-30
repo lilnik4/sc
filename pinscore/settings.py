b6fdfd5f603146ce81d6b0ce62adfea16ab055ba
Nima Karimi <nima.karimi@live.com>
2018-11-30 22:24:54 +0330

    dep5

--- pinscore/settings.py
+++ pinscore/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q+@4aj%vwkrhfjpppuh0a=c3so^a$eewt&*sb5d_uov!98(ali'
 DEBUG = True
-ALLOWED_HOSTS = ['pinscore.live', 'www.pinscore.live']
+ALLOWED_HOSTS = ['pinscore.live', 'www.pinscore.live','87.236.209.216']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -80,4 +80,3 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
